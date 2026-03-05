fabe26619ade32e3ec4b8c5163d49b478287af49
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2026-03-05 14:44:18 -0300

    switch case

---
+++ 6 switch-case.js
@@ -0,0 +1,31 @@
+let opcao=3 
+// let-variavel 
+// const-constante
+
+// tendo dois switch voce vai no segundo pois tem o expansor de abreviaçao ou seja te dar o formato pronto asssim como ! de html
+// match case em python em javascrpit e em java é switch case 
+switch (opçao) {
+    case 1:
+        console.log("opção 1")
+        break; 
+    case 2:
+        console.log("opção 1")
+        break; 
+    case 3:
+        console.log("opção 1")
+        break;
+    
+    case 4:
+        console.log("opção 1")
+        break;
+    
+    case 5:
+        console.log("opção 1")
+        break;
+    
+    default:
+        break;
+    
+
+        
+}
