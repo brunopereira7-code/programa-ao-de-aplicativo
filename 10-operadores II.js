243399ad16967925d22547bc3964037e763e4e1f
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2026-03-05 16:46:39 -0300

    study as alteraçoes

--- 10-operadores II.js
+++ 10-operadores II.js
@@ -1,3 +1,13 @@
 // operadores logicos 
 
-condicao_a= 1 <2
+condicao_a= 1 <2 
+condicao_b= 10< 2 
+
+ console.log("--and-") 
+ console.log(condicao_a && condicao_b) 
+
+ console.log("-not-") 
+ console.log(condicao_a && !condicao_b)
+
+ console.log("-or-") 
+ console.log(condicao_a || condicao_b)
