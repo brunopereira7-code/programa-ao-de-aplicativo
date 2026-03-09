8dc1f70ed24a33f7a19fd05139bf4c38b7f4f5e2
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2026-03-09 15:29:56 -0300

    update assunto

---
+++ 14 vetores.js
@@ -0,0 +1,8 @@
+const lista_nomes=['bruno','joao','marilia','fernando']
+console.log("exibindo todos os elementos") 
+console.log(lista_nomes)
+
+console.log("adicionar elementos") 
+lista_nomes.push("creuza")
+console.log(lista_nomes) 
+
