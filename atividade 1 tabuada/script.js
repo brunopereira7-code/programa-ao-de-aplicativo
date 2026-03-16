fedd27ea826448c60ee9474e752665a5727490c1
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2026-03-16 16:33:58 -0300

    atualizaçoes do study

--- atividade 1 tabuada/script.js
+++ atividade 1 tabuada/script.js
@@ -2,8 +2,11 @@ function gerarTabuada(){
     const numeroInput=document.getElementById("numeroInput");
     let numero=parseInt(numeroInput.value);
 
+/* ---------------------------------------------------------------------------------------------------------------------- */
     const resultadoDiv=document.getElementById("resultadoTabuada");
     resultadoDiv.innerHTML="";
+    
+/* ---------------------------------------------------------------------------------------------------------------------- */
 
     if(isNaN(numero) || numero === null || numero ===""){
         resultadoDiv.innerHTML="<p>por favor digite um numero valido</p>"
@@ -11,18 +14,24 @@ function gerarTabuada(){
 
 
     }
+    
+/* ---------------------------------------------------------------------------------------------------------------------- */
     //adicionar titulo na tabuada
     resultadoDiv.innerHTML +=`<h2>tabuada do numero ${numero}</h2>`;
+    
+/* ---------------------------------------------------------------------------------------------------------------------- */
 
     //laço de repetiçao pra gerar tabuada
     for (let i = 1; i <= 10; i++) {
         let resultado= numero * i;
         resultadoDiv.innerHTML += `<p>${numero} x ${i}= ${resultado}</p>` ;
 
+
         
     }
 
 }
 
+/* ---------------------------------------------------------------------------------------------------------------------- */
 const botaoGerar=document.getElementById("btngerar");
 botaoGerar.addEventListener("click",gerarTabuada);
